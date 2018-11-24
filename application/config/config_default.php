155da7dbdb89592f1cd818af54ffefc571976b49
Edwin van Egmond <edwinvanegmond@gmail.com>
2018-11-24 13:23:26 +0100

    remove database.php and config.php in /application/config/ and restore CodeIgniter's versions to *_default.php

--- application/config/config_default.php
+++ application/config/config_default.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'Y7ARwKPdceZ1Banxbajv1SjFpPNCHyrCy2BWTdOP';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database_default.php
+++ application/config/database_default.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'vsftpdweb';
-$db['default']['password'] = 'vsftpdadmin';
-$db['default']['database'] = 'vsftpd';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
