5e711674cf28d5beb886ed273ef33bf35c799ca8
Edwin van Egmond <edwinvanegmond@gmail.com>
2018-11-24 16:54:22 +0100

    format application/models/

--- application/models/log_model.php
+++ application/models/log_model.php
@@ -1,23 +1,19 @@
 <?php
-class Log_model extends CI_Model {
 
+class Log_model extends CI_Model
+{
 	public function __construct()
 	{
-		
 		$this->load->database();
 		parent::__construct();
 	}
-	
 
-	public function get_settings($id) {
-	
-	$query = $this->db->get_where('settings', array('id' => $id));
+	public function get_settings($id)
+	{
+		$query = $this->db->get_where('settings', array('id' => $id));
 
-	$query = $query->row_array(0);
-	$query1 = $query['value'];
-	return $query1;
+		$query = $query->row_array(0);
+		$query1 = $query['value'];
+		return $query1;
 	}
-
 }
-
-?>
