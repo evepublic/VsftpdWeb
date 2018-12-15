7d6cc6e01bfd4beda2d1944286ce1e6616c42ee0
Edwin van Egmond <edwinvanegmond@gmail.com>
2018-12-15 14:49:03 +0100

    add style sheets and js to <head>

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -4,4 +4,9 @@
 	<link rel="stylesheet" href="<?= base_url(public_path() . 'css/960.css'); ?>" type="text/css" media="screen" charset="utf-8">
 	<link rel="stylesheet" href="<?= base_url(public_path() . 'css/template.css'); ?>" type="text/css" media="screen" charset="utf-8">
 	<link rel="stylesheet" href="<?= base_url(public_path() . 'css/colour.css'); ?>" type="text/css" media="screen" charset="utf-8">
+
+	<link rel="stylesheet" href="<?= base_url(public_path() . 'css/bootstrap.min.css'); ?>">
+
+	<script src="<?= base_url(public_path() . 'js/bootstrap.min.js'); ?>"></script>
+	<script src="<?= base_url(public_path() . 'js/jquery-2.2.4.min.js'); ?>"></script>
 </head>
